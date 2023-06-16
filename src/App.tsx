4a3382ad09c919ae8d323c24abd9297ab9e8cf91
Ajirthan <ajirbala28@gmail.com>
2023-06-16 18:00:49 +0530

    feat(Footer): implement Footer component
    feat(Footer): implement Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -12,13 +13,7 @@ function App() {
                     <div className="bg-green-400 h-[30rem] w-[30rem] aspect-square flex items-center justify-center">Board</div>
                     <div className="bg-red-400 h-[40rem] w-72 aspect-square flex items-start justify-center">Move History</div>
                 </div>
-                <div className="flex justify-start items-center w-full h-20">
-                    <div className="flex justify-center items-center h-full w-[30rem] bg-red-600">
-                        <div className="flex justify-start items-center h-full w-1/3 bg-orange-500">P1 Score</div>
-                        <div className="flex justify-center items-center h-full w-1/3 bg-blue-500">Ties:</div>
-                        <div className="flex justify-end items-center h-full w-1/3 bg-orange-500">P2 Score</div>
-                    </div>
-                </div>
+                <Footer/>
             </div>
         </div>
     );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,33 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="flex justify-center items-center w-full h-20">
-            <div className="flex justify-start items-center h-full w-1/3 bg-orange-500">P1 Score</div>
-            <div className="flex justify-center items-center h-full w-1/3 bg-blue-500">Ties:</div>
-            <div className="flex justify-end items-center h-full w-1/3 bg-orange-500">P2 Score</div>
+        <div className="flex justify-start items-center w-full h-20">
+            <div className="flex justify-center items-center h-full w-[30rem]">
+                <div className="flex justify-start items-center h-full w-1/3">
+                    <div className="flex justify-start items-center w-36 h-16 ml-2">
+                        <div
+                            className="bg-blue-400 hover:bg-blue-500 text-slate-900 font-bold py-2 px-4 rounded-2xl inline-flex items-center justify-center w-full h-full">
+                            P1: 0
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-center items-center h-full w-1/3">
+                    <div className="flex justify-start items-center w-36 h-16 ml-2">
+                        <div
+                            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-2xl inline-flex items-center justify-center w-full h-full">
+                            Ties: 0
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-end items-center h-full w-1/3">
+                    <div className="flex justify-start items-center w-36 h-16 mr-2">
+                        <div
+                            className="bg-yellow-400 hover:bg-yellow-500 text-slate-900 font-bold py-2 px-4 rounded-2xl inline-flex items-center justify-center w-full h-full">
+                            P2: 0
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
